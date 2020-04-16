b3d7d789aa9e46a72166a917d18ed54026f3a806
Kat Law <kathleenmlaw1@gmail.com>
2020-04-16 16:00:13 -0400

    setting up express server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express =require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/ping', (req, res) => {
+    res.send('pong')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
